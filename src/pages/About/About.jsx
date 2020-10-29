d3e7225bbd19cbf7dc8b97ee291e62dbf3abc21b
agne-zavackyte <a.zavackyte@gmail.com>
2020-10-29 18:45:44 +0200

    Abour page content added

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,13 +1,43 @@
-import React from "react";
-import { Section } from "../../components/";
+import React, { useContext, useState, useEffect } from "react";
+import { Section, Button } from "../../components/";
+import { LocationContext } from "../../contexts/location.context";
+import { useHistory, Link } from "react-router-dom";
+import * as S from "./About.style";
 
 function About() {
+  const location = useContext(LocationContext);
+  const history = useHistory();
+
+  const [set, setData] = useState();
+
+  if (!location.state) {
+    history.push("/");
+  }
+
+  useEffect(() => {
+    if (location.state) {
+      fetch(`http://localhost:3000/${location.state}`)
+        .then((res) => res.json())
+        .then((data) => setData(data));
+    }
+  }, [location.state]);
+
   return (
     <Section>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-      laboriosam incidunt explicabo laudantium pariatur quae, dolore, impedit
-      dignissimos, architecto voluptatibus saepe quisquam repudiandae excepturi
-      fugiat numquam assumenda ut cumque labore.
+      <S.HeadingBlock>
+        <h1>Product List</h1>
+        <Link to="/">
+          <Button>Go Back</Button>
+        </Link>
+      </S.HeadingBlock>
+      <ul>
+        {set &&
+          set
+            .filter((item) => item.city.toLowerCase().includes(location.state))
+            .map((fitem) => (
+              <li key={fitem.id}>{fitem.product + " " + fitem.about}</li>
+            ))}
+      </ul>
     </Section>
   );
 }
