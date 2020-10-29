db8a7461e095d2d9d97dfa5dea3488f07ebe0177
agne-zavackyte <a.zavackyte@gmail.com>
2020-10-29 18:45:18 +0200

    Section stories fix

--- src/components/Section/Section.stories.jsx
+++ src/components/Section/Section.stories.jsx
@@ -1,29 +1,37 @@
 import { storiesOf } from "@storybook/react";
 import React from "react";
 import Section from "./Section";
+import theme from "../../theme";
+import { ThemeProvider } from "styled-components";
 
 storiesOf("Section", module)
   .add("Full-width Section", () => (
-    <Section background="eee" fullWidth={true}>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat optio
-      cumque vel, porro saepe voluptatem aliquid consequatur, sed dignissimos,
-      dicta in illo officiis pariatur. Pariatur maxime molestiae reiciendis odit
-      autem!
-    </Section>
+    <ThemeProvider theme={theme}>
+      <Section background="eee" fullWidth={true}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat optio
+        cumque vel, porro saepe voluptatem aliquid consequatur, sed dignissimos,
+        dicta in illo officiis pariatur. Pariatur maxime molestiae reiciendis
+        odit autem!
+      </Section>
+    </ThemeProvider>
   ))
   .add("Fixed-width Section", () => (
-    <Section background="eee" fullWidth={false}>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat optio
-      cumque vel, porro saepe voluptatem aliquid consequatur, sed dignissimos,
-      dicta in illo officiis pariatur. Pariatur maxime molestiae reiciendis odit
-      autem!
-    </Section>
+    <ThemeProvider theme={theme}>
+      <Section background="eee" fullWidth={false}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat optio
+        cumque vel, porro saepe voluptatem aliquid consequatur, sed dignissimos,
+        dicta in illo officiis pariatur. Pariatur maxime molestiae reiciendis
+        odit autem!
+      </Section>
+    </ThemeProvider>
   ))
   .add("Fixed-width-Transparent Section", () => (
-    <Section fullWidth={false}>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat optio
-      cumque vel, porro saepe voluptatem aliquid consequatur, sed dignissimos,
-      dicta in illo officiis pariatur. Pariatur maxime molestiae reiciendis odit
-      autem!
-    </Section>
+    <ThemeProvider theme={theme}>
+      <Section fullWidth={false}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat optio
+        cumque vel, porro saepe voluptatem aliquid consequatur, sed dignissimos,
+        dicta in illo officiis pariatur. Pariatur maxime molestiae reiciendis
+        odit autem!
+      </Section>
+    </ThemeProvider>
   ));
